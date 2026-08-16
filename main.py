9b501806c05b26753cf4a50e965809364fabf0e9
aberdin1208-droid <aberdin1208@gmail.com>
2026-08-16 18:56:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import os
-import logging
 import threading
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("TOKEN")
+flask_app = Flask(__name__)
 
-app = Flask(__name__)
-@web.route('/')
+@flask_app.route('/')
 def home():
-    return "Bot Aimos online!"
+    return "OK - Bot Aimos online"
 
 def run_flask():
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    flask_app.run(host="0.0.0.0", port=port)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot online 100% free!")
+    await update.message.reply_text("Finalmente online!")
 
 def main():
     if not TOKEN:
         print("ERRO: TOKEN nao encontrado")
         return
     threading.Thread(target=run_flask, daemon=True).start()
-    bot = ApplicationBuilder().token(TOKEN).build()
-    bot.add_handler(CommandHandler("start", start))
-    bot.run_polling()
+    print("Bot iniciando...")
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
